7078f5f93fa31ecf5bb87d38487c6ffca5f7ae04
THE_GODFATHERR_X <mm1miq.d@gmail.com>
2023-02-28 10:50:58 +0530

    Update INDORI_027.c

--- INDORI_027.c
+++ INDORI_027.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
+int fact(n) {
+	
+	if (n==1) {
+		return 1;
+	}
+	else { 
+	return n*fact(n-1);
+	}
+}
+
 int main() {
 
-int N;
+int N, fct;
 printf("Enter N: ");
 scanf("%d", &N);
+fct=fact(N);
+printf("factorial:%d\n",fct);
 
-int fact=1;
-for (int i=1; i<=N; i++) {
-fact=fact*i;
 }
-printf("factorial:%d\n",fact);
-}
+
+
+
