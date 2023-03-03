57e20bc4eb7d4944a998777edc8e1d9abf9926c7
THE_GODFATHERR_X <mm1miq.d@gmail.com>
2023-03-03 10:19:25 +0000

    3/3 march

---
+++ .ccls-cache/@home@runner@SCET-2022-C-programming-FY/INDEX.c
@@ -0,0 +1,59 @@
+
+// 1 scan 2 nums and add, sub, mult, div them.
+// 2 area of triangle
+// 3 write a progam to calculate simple interest
+
+// 4 write a program to compute farenheit from centigrade
+// f = (1.8 * c) + 32
+
+// 5 write a program to enter distance in KM,
+// convert to meter, feet, inches, CM
+
+// 6 write a program to find out, distance traveled by a vehicle, for time T
+
+// with eqn, dist = u*t + a*t/2 , where u is intital velocity.
+
+// 8 write a program to find whether the givwen number is postive, negative or zero
+// 10 write a program to read marks, of a single subject, if marks < 36, output the result
+// 7 write a program to find max among 3 nums.
+// 9 write a program to check, the entered char is cap,small or digit, or special char.
+
+// 11 enter marks, marks between 100 and 80: "distinction"
+// 79-60 ""1st class"
+// 59 to 40 "2nd class"
+// less than 40, "fail"
+
+// 12 write a program to prepare, a pay slip, with following data.
+// DA=10% of basic salary, HRA = 7.5% of basic, MA=300, PF = 3.5% of basic, GROSS SALARY= basic+DA+HRA+MA
+
+// 13 write a program to read number 1 to 7 and bring relatively day sunday to satuday USING SWITCH CASE
+
+// 14 write a menu driven program for sceintific calculator using switch case statement
+
+// 15 write a program to cehck the entered caracter is vowel or not using switch case
+
+// 16 sum of first N nos
+
+// 17 generate N fibb sequence
+
+// 18 n factorial
+
+// 19 MIN, MAX, AVG of 4 nums
+
+// 20 Swap value of 2 variables 
+
+// 21 Sum of first N integers
+
+// 22 check if last digit of num is odd/even.
+
+// 23 scan 10 nums and print MAX
+
+// 24 scan 10 nums and print MIN 
+
+// 25 Write a program to check whether the given number is prime or not
+
+// 26 generate primes till N
+
+// 27 find factorial of given number, recursive
+
+// 28 accept a string, and count number of characters
---
+++ INDORI_028.c
@@ -0,0 +1,16 @@
+// 26 Write a program that accept a string and count the number of space character, tab character,
+// new line character, and any other characters.
+
+#include<stdio.h>
+void main() {
+
+	int i;
+	char str[100];
+	printf("enter string: ");
+	scanf("%s", str);
+	for (i=0;str[i]!='\0';i++){
+		printf("%d", i);
+		  }
+
+
+}
